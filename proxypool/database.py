7b2fcf145a674b4fa6adeb970e118d1927bf8fad
fgksgf <fgksgf@yahoo.com>
2019-03-18 15:42:32 +0800

    修改注释格式

--- proxypool/database.py
+++ proxypool/database.py
@@ -21,6 +21,7 @@ class RedisClient:
     def add_proxies(self, proxies, score=INITIAL_SCORE):
         """
         使用管道，批量向数据库中添加代理数据，将其分数设置为最高
+
         :param proxies: 代理
         :param score: 分数
         :return: 添加结果
@@ -34,6 +35,7 @@ class RedisClient:
     def random_get_proxy(self):
         """
         随机获取有效代理，首先尝试获取最高分数代理，如果不存在，按照排名获取，否则异常
+
         :return: 随机代理
         """
         # 获取分数大于等于100的代理
@@ -48,21 +50,24 @@ class RedisClient:
             else:
                 raise PoolEmptyError
 
-    def degrade_proxy(self, proxy):
+    def degrade_proxy(self, proxy, count=DECREASE_SCORE):
         """
-        代理值减一分，小于最小值则删除
+        代理值减分，小于最小值则删除
+
         :param proxy: 代理
+        :param count: 要扣的分
         :return: 修改后的代理分数
         """
         score = self.db.zscore(REDIS_KEY, proxy)
         if score and score > MIN_SCORE:
-            return self.db.zincrby(REDIS_KEY, DECREASE_SCORE, proxy)
+            return self.db.zincrby(REDIS_KEY, count, proxy)
         else:
             return self.db.zrem(REDIS_KEY, proxy)
 
     def exists(self, proxy):
         """
         判断数据库中是否存在某代理
+
         :param proxy: 代理
         :return: 是否存在，
         """
@@ -71,6 +76,7 @@ class RedisClient:
     def set_max_score(self, proxy):
         """
         将代理设置为MAX_SCORE
+
         :param proxy: 代理
         :return: 设置结果
         """
@@ -79,6 +85,7 @@ class RedisClient:
     def get_proxy_count(self):
         """
         获取数据库中的代理数量
+
         :return: 数量
         """
         return self.db.zcard(REDIS_KEY)
@@ -86,6 +93,7 @@ class RedisClient:
     def get_all_ascending(self):
         """
         获取全部代理，按分数升序
+
         :return: 全部代理列表
         """
         return self.db.zrangebyscore(REDIS_KEY, MIN_SCORE, MAX_SCORE)
@@ -93,6 +101,7 @@ class RedisClient:
     def get_batch(self, start, stop):
         """
         从数据库中批量获取代理
+
         :param start: 开始索引
         :param stop: 结束索引
         :return: 代理列表
@@ -102,6 +111,7 @@ class RedisClient:
     def get_all_available(self):
         """
         从数据库中获取所有可用代理
+
         :return: 代理列表
         """
         return self.db.zrangebyscore(REDIS_KEY, MAX_SCORE, '+inf')
