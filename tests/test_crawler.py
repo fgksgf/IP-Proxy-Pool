78a5ee8bc4e6d04e53e28d00b559c09a52058479
Hoshea <fgksgf@gmail.com>
2020-01-31 20:32:39 +0800

    add comments

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -5,15 +5,26 @@ from proxypool.crawler import Crawler
 
 # TODO: Use decorator to refactor the code
 class CrawlerTest(unittest.TestCase):
+    """代理获取爬虫测试类
+
+    测试各个代理源网站能否正常获取代理
+    """
+
     crawler = Crawler()
 
     @staticmethod
     def get_number_of_proxies(proxies):
+        """
+        统计生成器中有效代理的数量
+
+        :param proxies: 生成器
+        :return: 代理数量
+        """
         count = 0
         try:
             p = next(proxies)
             while p:
-                if re.match(r'\d+\.\d+\.\d+\.\d+\:\d+', p):
+                if re.match(r'\d+\.\d+\.\d+\.\d+:\d+', p):
                     count += 1
                     break
                 p = next(proxies)
@@ -35,10 +46,6 @@ class CrawlerTest(unittest.TestCase):
 
     def test_crawl_xiaohuan(self):
         proxies = self.crawler.crawl_xiaohuan()
-        self.assertGreater(self.get_number_of_proxies(proxies), 0)
-
-    def test_crawl_proxylist(self):
-        proxies = self.crawler.crawl_proxylist(max_page=1)
         self.assertGreater(self.get_number_of_proxies(proxies), 0)
 
 
