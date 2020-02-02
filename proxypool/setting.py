cae5cfc98a6afee723c81456abf21cd65e289325
Hoshea <fgksgf@gmail.com>
2020-02-02 11:15:10 +0800

    add comments

--- proxypool/setting.py
+++ proxypool/setting.py
@@ -7,6 +7,7 @@ REDIS_PORT = 6379
 # Redis密码，如无填None
 REDIS_PASSWORD = 'password'
 
+# redis中存储代理的键名
 REDIS_KEY = 'proxies'
 
 # 代理分数
@@ -15,13 +16,12 @@ MIN_SCORE = 0
 INITIAL_SCORE = 10
 DECREASE_SCORE = -1
 
-VALID_STATUS_CODES = [200, ]
-
 # 代理池数量界限
 POOL_UPPER_THRESHOLD = 10000
 
 # 检查周期：一小时
 TESTER_CYCLE = 60 * 60
+
 # 获取周期：三小时
 GETTER_CYCLE = 60 * 60 * 3
 
@@ -33,10 +33,10 @@ TEST_URL = 'http://httpbin.org/ip'
 API_HOST = '0.0.0.0'
 API_PORT = 5000
 
-# 开关
+# 代理测试器、代理获取器、API模块启用开关
 TESTER_ENABLED = True
 GETTER_ENABLED = True
 API_ENABLED = True
 
-# 最大批测试量
+# 代理测试器最大批测试量
 BATCH_TEST_SIZE = 30
