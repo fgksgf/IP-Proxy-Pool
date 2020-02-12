d6a4e247fb933d063aab2828478af39074369f22
Hoshea <fgksgf@gmail.com>
2020-02-12 17:38:43 +0800

    V 1.1

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,7 +5,7 @@ from proxypool.database import RedisClient
 
 class TestRedisClient(TestCase):
     def test_add_proxies(self):
-        redis = RedisClient()
+        redis = RedisClient(host='127.0.0.1')
         test_data = [str(i) for i in range(100)]
         ret = redis.add_proxies(test_data, key='test')
         self.assertEqual(len(test_data), ret)
